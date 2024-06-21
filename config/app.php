0e68ab1b40f1dcb43a8abf23aa8d1aa624b9225e
Patel Shakil <100054724+PatelShakil@users.noreply.github.com>
2024-06-21 09:52:14 +0530

    TimeZone

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'IST'),
+    'timezone' => env('APP_TIMEZONE', 'Asia/Kolkata'),
 
     /*
     |--------------------------------------------------------------------------
